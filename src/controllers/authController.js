7838c2e48647c437c54b5232ccbd4eabbdb99a0f
GAURAV KATARA <gauravkatara53@gmail.com>
2025-06-26 13:13:23 +0530

    fix cookies

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -172,11 +172,11 @@ export const googleLoginSuccess = async (req, res) => {
     user._id
   );
 
-  // Cookie Options (same as your normal login)
+  // Cookie Options
   const options = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Secure only in production
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // 'None' only when using HTTPS
+    secure: true,
+    sameSite: "None",
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   };
